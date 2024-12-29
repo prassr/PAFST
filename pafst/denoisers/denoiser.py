8a4f73bf11aa0b8d0fa976341d6de0debe5f0f4d
prassr <92257326+prassr@users.noreply.github.com>
2024-12-29 10:01:58 +0530

    added pafst

---
+++ pafst/denoisers/denoiser.py
@@ -0,0 +1,63 @@
+import torchaudio
+from denoiser import pretrained
+from denoiser.dsp import convert_audio
+
+from pafts.datasets.dataset import Dataset
+
+from df.enhance import enhance, init_df, load_audio, save_audio
+from df.utils import download_file
+import soundfile as sf
+
+
+
+def dfn3_denoise(in_audio_path, out_audio_path, model, df_state):
+    audio, _ = load_audio(in_audio_path, sr=df_state.sr())
+    enhanced = enhance(model, df_state, audio).detach().cpu().numpy()
+    save_audio(out_audio_path, enhanced.detach().cpu().numpy(), 16000)
+    # return enhanced, df_state.sr()
+
+def fb_denoise(in_audio_path, out_audio_path, model):
+    wav, sr = torchaudio.load(in_audio_path)
+    wav = convert_audio(wav.cuda(), sr, model.sample_rate, model.chin)
+    with torch.no_grad():
+        denoised = model(wav[None])[0]
+    sf.write(out_audio_path, denoised.data.cpu().numpy().squeeze(), model.sample_rate)
+    # return denoised.data.cpu().numpy().squeeze(), sr
+
+
+def load_model(processor):
+    if processor=="dfn":
+        model, df_state, _ = init_df()
+        return model, df_state, dfn3_denoise
+    elif processor=="den":
+        model = pretrained.dns64().cuda()
+        return model, None, fb_denoise
+    else:
+        # g_c_n
+        pass
+    
+
+def denoiser(dataset: Dataset, processor="dfn"): # den for facebook
+    audios = dataset.audios
+    new_audios = []
+
+    model, df_state, proc_fun = load_model(processor)
+
+    for audio in audios:
+        
+        in_audio_path=str(audio)
+        file_name = audio.with_name(f'{audio.stem}_{i}{audio.suffix}')
+        file_name = file_name.name
+        
+        out_audio_path = (dataset.output_path / file_name).resolve()
+    
+        if processor == "dfn":
+            proc_fun(in_audio_path, out_audio_path, model, df_state)
+        elif processor == "den":
+            proc_fun(in_audio_path, out_audio_path, model)
+        
+        new_audios.append(out_audio_path)
+
+    dataset.audios = new_audios
+
+    return new_audios
---
+++ pafst/stt/stt.py
@@ -0,0 +1,98 @@
+import json
+from pathlib import Path
+from collections import defaultdict
+
+import whisper
+from whisper.tokenizer import LANGUAGES
+from tqdm import tqdm
+
+from pafts.datasets.dataset import Dataset
+
+whisper_model = {key: None for key in whisper._MODELS}
+
+
+def whisper_stt(
+        audio: Path,
+        model_size='base',
+        language=None,
+):
+    """
+        Use the Whipser[https://github.com/openai/whisper] STT model to extract text.
+        If there is no gpu or low performance, use the base model.
+
+        Args:
+            audio (Data): Audio data.
+            model_size (str): Size of the whisper model.
+            language (str): Language of the audio file to run STT.
+
+        Return:
+            str: Text in an audio file.
+
+        """
+    global whisper_model
+    if model_size not in whisper_model.keys():
+        raise ValueError(
+            "[!] Invalid model size selected. Please choose one of the following sizes: tiny, base, small, medium, large.")
+
+    if not whisper_model[model_size]:
+        whisper_model[model_size] = whisper.load_model(model_size)
+
+    if language: 
+        if language not in LANGUAGES:
+            raise ValueError(
+            f"[!] This language is not supported. Please select one of the language codes below\n{LANGUAGES}")
+
+        result = whisper_model[model_size].transcribe(str(audio), language=language)
+    else:
+        result = whisper_model[model_size].transcribe(str(audio))
+
+    return result['text']
+
+
+def STT(
+        dataset: Dataset,
+        output_format='json',
+        model_size='base',
+        language=None
+):
+    """
+    Read the audio files in the dataset, and use the stt function to extract text.
+    Save the extracted text in the form of a json file.
+
+    Args:
+        dataset (Dataset): Audio dataset Class
+        output_format (str): Output format, Defaults is json (json or txt)
+        model_size (str): Size of the whisper model.
+        language (str): Language of the audio file to run STT.
+
+    Returns:
+        Dict: Dictionary of the text values of audio files in the dataset.
+
+    """
+    audios = dataset.audios
+    stt_dict = defaultdict(dict)
+
+    output_path = dataset.output_path
+
+    bar = tqdm(audios,
+               total=len(audios),
+               leave=True,
+               )
+
+    for audio in bar:
+        text = whisper_stt(audio, model_size, language)
+        stt_dict[audio.parent.name][audio.name] = text
+
+    for speaker in stt_dict:
+        if output_format == 'json':
+            with open(Path(output_path) / Path(f'{speaker}.json'), 'w', encoding='UTF8') as f:
+                json.dump(stt_dict[speaker], f, indent=4, ensure_ascii=False)
+        elif output_format == 'txt':
+            with open(Path(output_path) / Path(f'{speaker}.txt'), 'w', encoding='UTF8') as f:
+                for k, v in stt_dict[speaker]:
+                    f.write(f'{k}|{v}\n')
+        else:
+            raise ValueError(
+                f"[!] Please choose one of the following format: json, txt.")
+
+    return stt_dict
