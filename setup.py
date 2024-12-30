1135eeee684147dfd1bf4604172898d0c98307d8
prassr <92257326+prassr@users.noreply.github.com>
2024-12-30 10:54:30 +0530

    modified setup

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("requirements.txt", 'r', encoding='utf-8') as f:
 
 setup(
     name='pafst',
-    version='1.0.1',
+    version='1.0',
     author='ashtavakra',
     author_email="vidyaaltar@gmail.com",
     description='Library That Preprocessing Audio For TTS/STT.',
     install_requires=requirements,
-    license='MIT License',
+    license='MIT',
     long_description=readme,
     long_description_content_type="text/markdown",
     url='https://github.com/prassr/PAFST',
@@ -25,9 +25,13 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.10',
-    ]
+    ],
+    keywords='speechrecognition asr voiceactivitydetection vad webrtc pafst audio denoising speaker diarization',
+    py_modules=['pafst']
 
 )
